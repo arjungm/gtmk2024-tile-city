e07fa2cf0cc9768084236f934860c50c2fe90291
Russell Cullen <russellcullen@nypl.org>
2024-08-18 10:46:38 -0400

    Set grid size and add bound checks

--- scenes/grid.gd
+++ scenes/grid.gd
@@ -2,12 +2,16 @@ extends TileMapLayer
 
 signal tile_placed(tile: Tile.Type)
 
-const BLANK_TILE_IDX = Vector2i(0, 6)
+@export var grid_size = 5
+var bounds = Rect2i(0, 0, grid_size, grid_size)
+const BLANK_TILE_IDX = Vector2i(15, 11)
 var placing_tile: Tile.Type = Tile.Type.UNKNOWN
-var preview: TileMapLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	for i in grid_size:
+		for j in grid_size:
+			set_cell(Vector2i(i, j), 1, BLANK_TILE_IDX)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,12 +19,15 @@ func _process(delta: float) -> void:
 	$PreviewLayer.clear()
 	if placing_tile != Tile.Type.UNKNOWN:
 		var preview_cell = $PreviewLayer.local_to_map(get_local_mouse_position())
-		$PreviewLayer.set_cell(preview_cell, 1, tile_to_index(placing_tile))
+		if bounds.has_point(preview_cell):
+			$PreviewLayer.set_cell(preview_cell, 1, tile_to_index(placing_tile))
 	pass
 
 func _input(event):
 	if event is InputEventMouseButton:
 		var cell = local_to_map(get_local_mouse_position())
+		if !bounds.has_point(cell):
+			pass
 		var curr_tile_index = get_cell_atlas_coords(cell)
 		if placing_tile != null && curr_tile_index == BLANK_TILE_IDX && event.button_index == MOUSE_BUTTON_LEFT:
 			set_cell(cell, 1, tile_to_index(placing_tile))
